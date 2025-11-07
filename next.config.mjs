1ca731f34d4b0aad5cf48cafd097ff0a368e8301
yaqioooong <yaqiongko@gmail.com>
2025-11-07 21:07:54 +0800

    build(docker): 添加 Docker 支持并配置 Next.js standalone 输出
    - 新增 .dockerignore 文件，排除不必要的构建文件
    - 创建多阶段 Dockerfile，优化镜像构建和运行
    - 配置 Next.js 为 standalone 模式以支持独立部署
    - 设置生产环境变量和用户权限确保安全运行

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        // destination: "http://8.153.82.10:8080/gq/api/:path*",
-        destination: "http://localhost:8080/gq/api/:path*",
+        destination: "http://8.153.82.10:8080/gq/api/:path*",
+        // destination: "http://localhost:8080/gq/api/:path*",
       },
     ];
   },
